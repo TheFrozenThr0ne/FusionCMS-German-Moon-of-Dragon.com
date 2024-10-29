3930b97d260c32b94c2c2c46ed16578fcb0d46c2
TheFrozenThr0ne <masterbaron@web.de>
2024-10-29 15:57:02 +0100

    App/Language: Add Swedish translation

---
+++ application/language/swedish/wow_constants.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * Översätt endast dessa om World of Warcraft gör det själva,
+ * annars kommer du förvirra människor som förväntar sig att se dem på engelska.
+ */
+
+$lang['races'] = array(
+    1 => "Människa",
+    2 => "Ork",
+    3 => "Dvärg",
+    4 => "Nattalf",
+    5 => "Onda",
+    6 => "Tauren",
+    7 => "Gnomo",
+    8 => "Troll",
+    9 => "Goblin",
+    10 => "Blodalf",
+    11 => "Draenei",
+    22 => "Worgen",
+    24 => "Pandaren",
+    25 => "Pandaren",
+    26 => "Pandaren",
+    27 => "Nattfödda",
+    28 => "Högbergetauren",
+    29 => "Voidalf",
+    30 => "Ljusförstärkt Draenei",
+    31 => "Zandalari Troll",
+    32 => "Kul Tiran",
+    34 => "Mörkjärnsdvärg",
+    35 => "Vulpera",
+    36 => "Mag'har Ork",
+    37 => "Mechagnom",
+    52 => "Dracthyr",
+    70 => "Dracthyr",
+    84 => "Jordväsende",
+    85 => "Jordväsende",
+);
+
+$lang['classes'] = array(
+    1 => "Krigare",
+    2 => "Paladin",
+    3 => "Jägare",
+    4 => "Rogue",
+    5 => "Präst",
+    6 => "Dödskalle",
+    7 => "Shaman",
+    8 => "Magiker",
+    9 => "Demonjägare",
+    10 => "Munk",
+    11 => "Druid",
+    12 => "Demonjägare",
+    13 => "Evoker"
+);
